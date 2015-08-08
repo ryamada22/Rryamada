465714886d22b48b423244f36f90f405b0d95691
Ryo Yamada <ryamada@genome.med.kyoto-u.ac.jp>
2015-08-08 18:10:09 +0900

    add momentX

--- R/ry_momentProb.R
+++ R/ry_momentProb.R
@@ -23,22 +23,3 @@ ry_momentProb<-function (x, p,order = 1, center = FALSE)
  sum(x^order*p)
 }
 
-
-#'
-#' This function returns order-th sample moment.
-#' @param x is a numeric vector.
-#' @param order is order of moment.
-#' @param center is logical; when TRUE, moment is around mean
-#' and when FALSE, around zero
-#' @keywords complex function, conformal transformation
-#' @export
-#' @examples
-#' n <- 10
-#' x <- runif(n)
-#' out2 <- ry_momentX(x,order=2,center=TRUE)
-
-
-momentX<-function (x, order = 1, center = FALSE) 
-{
- momentProb(x,p=rep(1,length(x))/length(x),order=order,center=center) 
-}
---
+++ R/ry_momentX.R
@@ -0,0 +1,19 @@
+#' Arbitrary moment
+#'
+#' This function returns order-th sample moment.
+#' @param x is a numeric vector.
+#' @param order is order of moment.
+#' @param center is logical; when TRUE, moment is around mean
+#' and when FALSE, around zero
+#' @keywords complex function, conformal transformation
+#' @export
+#' @examples
+#' n <- 10
+#' x <- runif(n)
+#' out2 <- ry_momentX(x,order=2,center=TRUE)
+
+
+ry_momentX<-function (x, order = 1, center = FALSE) 
+{
+ momentProb(x,p=rep(1,length(x))/length(x),order=order,center=center) 
+}
